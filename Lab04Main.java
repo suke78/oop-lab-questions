39d302a3772bae780ea2db651fafd647433441cf
Sukeshi Thayaparan <sukeshithayaparan@gmail.com>
2026-04-15 23:37:14 +0530

    lab04 q2 lecturer child class

--- Lab04Main.java
+++ Lab04Main.java
@@ -1,1 +1,8 @@
+public class Lab04Main {
+    public static void main(String[] args) {
+        Lecturer lec = new Lecturer("De Silva", "L01", "IT", 4, 50000);
 
+        lec.displayLecturerDetails();
+        System.out.println("Monthly Payment: " + lec.calculateMonthlyPayment());
+    }
+}
