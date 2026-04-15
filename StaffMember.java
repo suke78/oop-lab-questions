879d54dba6fc9b6e0f8c59ad3fcc4609f39f6b21
Sukeshi Thayaparan <sukeshithayaparan@gmail.com>
2026-04-15 23:02:55 +0530

    lab04 q1 abstract parent created

--- StaffMember.java
+++ StaffMember.java
@@ -1,1 +1,42 @@
 
+abstract class StaffMember {
+    private String fullName;
+    private final String staffId;
+    protected String department;
+
+    // Constructor
+    public StaffMember(String fullName, String staffId, String department) {
+        this.fullName = fullName;
+        this.staffId = staffId;
+        this.department = department;
+    }
+
+    // Getters
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getStaffId() {
+        return staffId;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    // Final method
+    public final void displayBasicDetails() {
+        System.out.println("Name: " + fullName);
+        System.out.println("Staff ID: " + staffId);
+        System.out.println("Department: " + department);
+    }
+
+    // Abstract method
+    public abstract double calculateMonthlyPayment();
+}
+
+/*
+Reason:
+StaffMember is abstract because it is a general design and should not be instantiated directly.
+Only child classes like Lecturer and LabAssistant should create objects.
+*/
