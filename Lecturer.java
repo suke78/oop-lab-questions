3400401ca3a510111aff07a41eb206874ed0c740
Sukeshi Thayaparan <sukeshithayaparan@gmail.com>
2026-04-15 23:29:33 +0530

    lab04 q2 lecturer child class

--- Lecturer.java
+++ Lecturer.java
@@ -1,1 +1,21 @@
+class Lecturer extends StaffMember {
+    private int courseCount;
+    private double paymentPerCourse;
 
+    public Lecturer(String fullName, String staffId, String department,
+                     int courseCount, double paymentPerCourse) {
+        super(fullName, staffId, department);
+        this.courseCount = courseCount;
+        this.paymentPerCourse = paymentPerCourse;
+    }
+
+    public double calculateMonthlyPayment() {
+        return courseCount * paymentPerCourse;
+    }
+
+    public void displayLecturerDetails() {
+        displayBasicDetails();
+        System.out.println("Courses: " + courseCount);
+        System.out.println("Payment per Course: " + paymentPerCourse);
+    }
+}
