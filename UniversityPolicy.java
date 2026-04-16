3ebec21bddb268b32346028415aa88d75c36aaae
Sukeshi Thayaparan <sukeshithayaparan@gmail.com>
2026-04-16 21:18:02 +0530

    lab04 q4 final policy class
    Added UniversityPolicy class with constants and methods for bonus calculation.

--- UniversityPolicy.java
+++ UniversityPolicy.java
@@ -1,1 +1,17 @@
+final class UniversityPolicy {
+    public static final String UNIVERSITY_NAME = "University of Kelaniya";
+    public static final double BONUS_RATE = 0.10;
 
+    public static void showPolicyHeader() {
+        System.out.println("=== University Payment Policy ===");
+    }
+
+    public static double calculateBonus(double monthlyPayment) {
+        return monthlyPayment * BONUS_RATE;
+    }
+}
+
+/*
+Reason:
+A final class should not be inherited to prevent modification of fixed rules.
+*/
