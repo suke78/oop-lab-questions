0faa255e73b7c246020356297d4d20bb8a0f1752
Sukeshi Thayaparan <sukeshithayaparan@gmail.com>
2026-04-16 21:08:43 +0530

    lab04 q3 second child class

--- LabAssistant.java
+++ LabAssistant.java
@@ -1,1 +1,26 @@
+class LabAssistant extends StaffMember {
+    private int hoursWorked;
+    private double hourlyRate;
 
+    public LabAssistant(String fullName, String staffId, String department,
+                        int hoursWorked, double hourlyRate) {
+        super(fullName, staffId, department);
+        this.hoursWorked = hoursWorked;
+        this.hourlyRate = hourlyRate;
+    }
+
+    public double calculateMonthlyPayment() {
+        return hoursWorked * hourlyRate;
+    }
+
+    public void displayLabAssistantDetails() {
+        displayBasicDetails();
+        System.out.println("Hours Worked: " + hoursWorked);
+        System.out.println("Hourly Rate: " + hourlyRate);
+    }
+}
+
+/*
+Reason:
+department is protected, so it can be accessed directly in child classes.
+*/
