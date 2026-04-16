444f64a63a8803d1a05178cbd4cc180b66933124
Sukeshi Thayaparan <sukeshithayaparan@gmail.com>
2026-04-16 22:51:37 +0530

    lab04 q6 final program completed

--- Lab04Main.java
+++ Lab04Main.java
@@ -29,3 +29,43 @@ public class Lab04Main {
     }
 }
 
+
+public class Lab04Main {
+    public static void main(String[] args) {
+
+        StaffMember.showSystemName();
+        UniversityPolicy.showPolicyHeader();
+
+        Lecturer l1 = new Lecturer("Ram", "L04", "IT", 2, 40000);
+        Lecturer l2 = new Lecturer("Bala", "L08", "CS", 3, 45000);
+        LabAssistant lab = new LabAssistant("Danial", "A02", "IT", 100, 500);
+
+
+        l1.changeDepartment("Engineering");
+
+        l1.displayLecturerDetails();
+        l2.displayLecturerDetails();
+        lab.displayLabAssistantDetails();
+
+        double total = 0;
+
+        double p1 = l1.calculateMonthlyPayment();
+        double p2 = l2.calculateMonthlyPayment();
+        double p3 = lab.calculateMonthlyPayment();
+
+        total = p1 + p2 + p3;
+
+        System.out.println("Payments:");
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+
+        System.out.println("Total Payment: " + total);
+        System.out.println("Total Staff: " + StaffMember.getStaffCount());
+
+        l1.showCommonNotice();
+        l2.showCommonNotice();
+        lab.showCommonNotice();
+    }
+}
+
