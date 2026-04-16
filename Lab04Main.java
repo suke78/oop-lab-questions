814596f823037605f2b2542efc6ec2824bffc4e0
Sukeshi Thayaparan <sukeshithayaparan@gmail.com>
2026-04-16 21:11:13 +0530

    lab04 q3 second child class

--- Lab04Main.java
+++ Lab04Main.java
@@ -6,3 +6,14 @@ public class Lab04Main {
         System.out.println("Monthly Payment: " + lec.calculateMonthlyPayment());
     }
 }
+
+public class Lab04Main {
+    public static void main(String[] args) {
+        Lecturer lec = new Lecturer("De Silva", "L01", "IT", 4, 50000);
+        LabAssistant lab = new LabAssistant("Ravikumar", "A01", "IT", 120, 500);
+
+        lec.displayLecturerDetails();
+        lab.displayLabAssistantDetails();
+    }
+}
+
