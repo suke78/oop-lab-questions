72ba36ebb5fe22ee74e734a0b8d2fbf33a842399
Sukeshi Thayaparan <sukeshithayaparan@gmail.com>
2026-04-16 22:06:04 +0530

    lab04 q5 static members added
    Added main method to Lab04Main class to demonstrate staff details.

--- Lab04Main.java
+++ Lab04Main.java
@@ -17,3 +17,15 @@ public class Lab04Main {
     }
 }
 
+public class Lab04Main {
+    public static void main(String[] args) {
+        StaffMember.showSystemName();
+
+        Lecturer l1 = new Lecturer("Ram", "L04", "IT", 2, 40000);
+        Lecturer l2 = new Lecturer("Bala", "L08", "CS", 3, 45000);
+        LabAssistant lab = new LabAssistant("Danial", "A02", "IT", 100, 500);
+
+        System.out.println("Total Staff: " + StaffMember.getStaffCount());
+    }
+}
+
